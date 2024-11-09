23c8ba9e6fbba31d692bec71ff0b98e76f18f75a
Pâmella Ranne <pamellaranne@gmail.com>
2024-11-09 15:16:57 -0300

    Adicionando a variável nivel

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 let nomeHeroi = "Pamella";
 let quantidadeExperiencia = 1000;
+let nivel;
 
 if (quantidadeExperiencia <= 1000) {
-    console.log("Classificação da Experiência: Ferro");
+    nivel = "Ferro";
 }
 else if (quantidadeExperiencia >= 1001 && quantidadeExperiencia <= 2000) {
-    console.log("Classificação da Experiência: Bronze");
+    nivel = "Bronze";
 }
 else if (quantidadeExperiencia >= 2001 && quantidadeExperiencia <= 5000) {
-    console.log("Classificação da Experiência: Prata");
+    nivel = "Prata";
 }
 else if (quantidadeExperiencia >= 5001 && quantidadeExperiencia <= 7000) {
-    console.log("Classificação da Experiência: Ouro");
+    nivel = "Ouro";
 }
 else if (quantidadeExperiencia >= 7001 && quantidadeExperiencia <= 8000) {
-    console.log("Classificação da Experiência: Platina")
+    nivel = "Platina";
 }
 else if (quantidadeExperiencia >= 8001 && quantidadeExperiencia <= 9000) {
-    console.log("Classificação da Experiência: Ascendente");
+    nivel = "Ascendente";
 }
 else if (quantidadeExperiencia >= 9001 && quantidadeExperiencia <= 10000) {
-    console.log("Classificação da Experiência: Imortal");
+    nivel = "Imortal";
 }
 else{
     console.log("Classificação da Experiência: Radiante");
 }
 
-console.log("O Herói de nome " + nomeHeroi + " está no nível de " + quantidadeExperiencia + ".");
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + ".");
